72179ae9724d245c54e1ed5c5026ba5b5f4a09ea
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-10-06 21:50:39 +0200

    a function that doubles numbers in an array

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const add = (x,y) => {
     return x + y;
 }
 
+/* This is an even more simplified version
+const add = (x,y) => x + y ;
+*/
+
 // A simple function which contains only 1 argument and returns itself
 const identity = x => x;
 
@@ -18,3 +22,9 @@ const greet = (name = 'stranger', greeting = 'Wassup?') => {
 }
 greet();                                                // Calling the function and leaving the default values
 greet('Tavonga');                                       // Inserting the name for a custom greeting
+
+// A function that doubles numbers in an array
+const doubledNumbers = (...numbers) => {
+    return numbers.map(x => x * 2);
+}
+console.log(doubledNumbers(1, 4, 13))                   // returns [2, 8, 26]
