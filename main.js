6e3ca7ebf0c931fe13ee9f1949c0450d92f9fe28
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-10-06 22:24:03 +0200

    formated code with prettier

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 // Creating a function in ES6
-const add = (x,y) => {
-    return x + y;
-}
+const add = (x, y) => {
+  return x + y;
+};
 
 /* This is an even more simplified version
 const add = (x,y) => x + y ;
 */
 
 // A simple function which contains only 1 argument and returns itself
-const identity = x => x;
+const identity = (x) => x;
 
 // Creating an array using a function
 const array = (...elements) => {
-    console.log(elements);
-}
-array('Jonathan', 23, 'basketball');
+  console.log(elements);
+};
+array("Jonathan", 23, "basketball");
 
 // Inserting default values on function parameters
-const greet = (name = 'stranger', greeting = 'Wassup?') => {
-    console.log(greeting, name);
-}
-greet();                                                // Calling the function and leaving the default values
-greet('Tavonga');                                       // Inserting the name for a custom greeting
+const greet = (name = "stranger", greeting = "Wassup?") => {
+  console.log(greeting, name);
+};
+greet(); // Calling the function and leaving the default values
+greet("Tavonga"); // Inserting the name for a custom greeting
 
 // A function that doubles numbers in an array
 const doubledNumbers = (...numbers) => {
-    return numbers.map(x => x * 2);
-}
-console.log(doubledNumbers(1, 4, 13))                   // returns [2, 8, 26]
+  return numbers.map((x) => x * 2);
+};
+console.log(doubledNumbers(1, 4, 13)); // returns [2, 8, 26]
